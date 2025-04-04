8febea483342c874655106429e6353395f26315f
Kryštof Jirsa <jirsakry@student.cvut.cz>
2025-04-04 13:17:37 +0000

    Update 14 files
    - /semestral/.idea/.gitignore
    - /semestral/.idea/encodings.xml
    - /semestral/.idea/misc.xml
    - /semestral/.idea/vcs.xml
    - /semestral/.mvn/wrapper/maven-wrapper.jar
    - /semestral/.mvn/wrapper/maven-wrapper.properties
    - /semestral/src/main/java/cz/cvut/fel/pjv/jirsakry/semestral/HelloApplication.java
    - /semestral/src/main/java/cz/cvut/fel/pjv/jirsakry/semestral/HelloController.java
    - /semestral/src/main/resources/cz/cvut/fel/pjv/jirsakry/semestral/hello-view.fxml
    - /semestral/src/main/java/module-info.java
    - /semestral/.gitignore
    - /semestral/mvnw
    - /semestral/mvnw.cmd
    - /semestral/pom.xml

--- semestral/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cz.cvut.fel.pjv.jirsakry.semestral {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens cz.cvut.fel.pjv.jirsakry.semestral to javafx.fxml;
-    exports cz.cvut.fel.pjv.jirsakry.semestral;
-}
